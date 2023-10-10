f973cc8e685a0d686d402ed7fc4a4566d5f5b2ae
atlejohansson <johanssonatle@gmail.com>
2023-10-10 18:08:05 +0200

    tog bort "redigera denna sida"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,15 +45,11 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/koderik/docusaurus-iare/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/koderik/docusaurus-iare/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
